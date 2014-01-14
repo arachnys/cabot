a2ca3f918e38bb7f879ff621547f285a3c0739ac
Alexander James Phillips <ajp.global@gmail.com>
2014-01-14 17:24:56 +0000

    Add link to jenkins job from service-checks dashboard

---
+++ app/cabotapp/templatetags/extra.py
@@ -0,0 +1,9 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.simple_tag
+def jenkins_human_url(jobname):
+  return '{}job/{}/'.format(settings.JENKINS_API, jobname)
