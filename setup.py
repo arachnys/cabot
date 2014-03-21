7f13321748e41c87543a659e53a297476a787f7e
Peter Baumgartner <pete@lincolnloop.com>
2014-03-21 12:58:53 -0600

    Adds certifi for an updated CA bundle for requests

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'anyjson==0.3.3',
         'billiard==3.3.0.13',
         'celery==3.1.7',
+        'certifi==1.0.1',
         'distribute==0.6.24',
         'dj-database-url==0.2.2',
         'django-appconf==0.6',
