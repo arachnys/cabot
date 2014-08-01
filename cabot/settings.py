8cf51d5bf6a0c078a7be6fc1b32332f483198443
Peter Baumgartner <pete@lincolnloop.com>
2014-08-01 09:36:46 -0600

    Fixes static paths

--- cabot/settings.py
+++ cabot/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, os.path.pardir, 'static/')
 
 COMPRESS_ROOT = STATIC_ROOT
 
@@ -65,7 +65,7 @@ COMPRESS_ROOT = STATIC_ROOT
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static')]
 
 # List of finder classes that know how to find static files in
 # various locations.
