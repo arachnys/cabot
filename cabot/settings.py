427f97bbceb51e596b8d8bc3e3c15cdd29865288
David Buxton <david@arachnys.com>
2014-08-05 00:09:06 +0100

    Remove transaction middleware

--- cabot/settings.py
+++ cabot/settings.py
@@ -92,7 +92,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.transaction.TransactionMiddleware',
 )
 
 ROOT_URLCONF = 'cabot.urls'
