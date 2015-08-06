bd580cf8f0ee1972e8993e4b495f5e9b0d804f9e
Mahendra <mahendra@affirm.com>
2015-08-06 14:02:07 -0700

    bug fixes

--- cabot/cabotapp/models.py
+++ cabot/cabotapp/models.py
@@ -710,6 +710,7 @@ class GraphiteStatusCheck(StatusCheck):
         if series['num_series_with_data'] < self.expected_num_hosts:
             failed = True
 
+        failed_metric_name = None
         matched_metrics = 0
 
         if self.expected_num_metrics > 0:
