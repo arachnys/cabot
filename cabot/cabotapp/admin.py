45449861503fdfc3a8dee04180803a3c057c505a
David Buxton <david@arachnys.com>
2015-10-02 10:27:15 +0200

    Update admin

--- cabot/cabotapp/admin.py
+++ cabot/cabotapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile, Service, Shift, ServiceStatusSnapshot, StatusCheck, StatusCheckResult, Instance
+from .models import (UserProfile, Service, Shift,
+    ServiceStatusSnapshot, StatusCheck, StatusCheckResult,
+    Instance, AlertAcknowledgement)
 from .alert  import AlertPluginUserData, AlertPlugin
 
 admin.site.register(UserProfile)
@@ -11,3 +13,4 @@ admin.site.register(StatusCheckResult)
 admin.site.register(Instance)
 admin.site.register(AlertPlugin)
 admin.site.register(AlertPluginUserData)
+admin.site.register(AlertAcknowledgement)
