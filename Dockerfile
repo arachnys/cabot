058a39c9a613e2470af2f17bfd44ead136c18fef
Jean-Fred Berthelot <jeanfred@arachnys.com>
2016-05-05 11:01:27 +0100

    Rework the Docker setup
    Also externalise requirements to pip format
    to benefit from Docker caching.

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,31 @@
 FROM python:2.7
+
 ENV PYTHONUNBUFFERED 1
+
 RUN mkdir /code
+
 WORKDIR /code
+
+RUN apt-get update && apt-get install -y \
+        python-dev \
+        libsasl2-dev \
+        libldap2-dev \
+        libpq-dev \
+        npm
+
+RUN npm install -g \
+        --registry http://registry.npmjs.org/ \
+        coffee-script \
+        less@1.3
+
+RUN ln -s `which nodejs` /usr/bin/node
+
+COPY requirements.txt ./requirements.txt
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY requirements-plugins.txt ./requirements-plugins.txt
+RUN pip install --no-cache-dir -r requirements-plugins.txt
+
+
 ADD . /code/
-RUN apt-get update && \
-  apt-get install -y \
-    python-dev \
-    libsasl2-dev \
-    libldap2-dev \
-    libpq-dev \
-    npm
-RUN npm install -g coffee-script less@1.3 --registry http://registry.npmjs.org/
-RUN [ ! -f /usr/bin/node ] && ln -s /usr/bin/nodejs /usr/bin/node
-RUN python setup.py install
--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
 from os import environ as env
+
+requirements_file = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+with open(requirements_file) as f:
+    requirements = [line.rstrip('\n')
+                    for line in f
+                    if line and not line.startswith('#')]
 
 # pull in active plugins
 plugins = env['CABOT_PLUGINS_ENABLED'].split(',') if 'CABOT_PLUGINS_ENABLED' in env else ["cabot_alert_hipchat", "cabot_alert_twilio", "cabot_alert_email"]
@@ -15,44 +22,7 @@ setup(
     author_email='info@arachnys.com',
     url='http://cabotapp.com',
     license='MIT',
-    install_requires=[
-        'Django==1.6.8',
-        'Markdown==2.5',
-        'PyJWT==0.1.2',
-        'South==1.0',
-        'amqp>=1.4.9',
-        'anyjson==0.3.3',
-        'argparse==1.2.1',
-        'celery==3.1.9',
-        'dj-database-url==0.2.2',
-        'django-appconf==0.6',
-        'django-celery==3.1.1',
-        'django-celery-with-redis==3.0',
-        'django-compressor==1.4',
-        'django-filter==0.7',
-        'django-jsonify==0.2.1',
-        'django-mptt==0.6.0',
-        'django-polymorphic==0.5.6',
-        'django-redis==1.4.5',
-        'django-smtp-ssl==1.0',
-        'djangorestframework==2.4.2',
-        'gunicorn==18.0',
-        'gevent==1.0.1',
-        'hiredis==0.1.1',
-        'httplib2==0.7.7',
-        'icalendar==3.2',
-        'mock==1.0.1',
-        'psycogreen==1.0',
-        'psycopg2==2.5.1',
-        'pytz==2014.10',
-        'redis==2.9.0',
-        'requests==2.7.0',
-        'six==1.5.1',
-        'twilio==3.4.1',
-        'wsgiref==0.1.2',
-        'python-dateutil==2.1',
-        'django-auth-ldap==1.2.6',
-    ] + plugins,
+    install_requires=requirements + plugins,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
