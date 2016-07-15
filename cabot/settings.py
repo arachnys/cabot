144db27a991f90387b7d067012050eff72778521
Mark Unsworth <mark@knowsis.com>
2016-07-15 13:40:43 +0100

    set default_from_email address - for non error emails sent by Django such as password reset. Fixes #360.

--- cabot/settings.py
+++ cabot/settings.py
@@ -16,6 +16,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+DEFAULT_FROM_EMAIL = os.environ.get('CABOT_FROM_EMAIL')
+
+
 DATABASES = {'default': dj_database_url.parse(os.environ["DATABASE_URL"])}
 
 if not DEBUG:
