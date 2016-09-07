98db3a660de8fedcf02ab00503f38de049ed3f8f
David Buxton <david@dbuxton.com>
2016-09-07 23:08:00 +0100

    Add docker entrypoints

---
+++ docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+echo "foo"
+echo $DEBUG
+
+# first check if we're passing flags, if so
+# prepend with sentry
+if [ "${1:0:1}" = '-' ]; then
+	set -- python manage.py "$@"
+fi
+
+case "$1" in
+"web")
+	set -- gunicorn cabot.wsgi:application --config gunicorn.conf
+	;;
+"beatworker")
+    set -- celery worker -B -A cabot --loglevel=INFO --concurrency=16 -Ofair
+    ;;
+"worker")
+    set -- celery worker -A cabot --loglevel=INFO --concurrency=16 -Ofair
+    ;;
+esac
+
+exec "$@"
