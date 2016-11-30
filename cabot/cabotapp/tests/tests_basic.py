2d0dbe15ec49715ab33ad06cb997ac555f58c947
Gregory Avery-Weir <gregory@futureproofgames.com>
2016-11-29 19:18:54 -0500

    Add tests for recurring calendar rotas

--- cabot/cabotapp/tests/tests_basic.py
+++ cabot/cabotapp/tests/tests_basic.py
@@ -170,6 +170,13 @@ def fake_gcal_response(*args, **kwargs):
     return resp
 
 
+def fake_recurring_response(*args, **kwargs):
+    resp = Mock()
+    resp.content = get_content('recurring_response.ics')
+    resp.status_code = 200
+    return resp
+
+
 def throws_timeout(*args, **kwargs):
     raise requests.RequestException(u'фиктивная ошибка innit')
 
@@ -432,6 +439,20 @@ class TestDutyRota(LocalTestCase):
     def test_duty_rota(self):
         events = get_events()
         self.assertEqual(events[0]['summary'], 'troels')
+
+    @patch('cabot.cabotapp.models.requests.get', fake_recurring_response)
+    def test_duty_rota_recurring(self):
+        events = get_events()
+        events.sort(key=lambda ev: ev['start'])
+        curr_summ = events[0]['summary']
+        self.assertTrue(curr_summ == 'foo' or curr_summ == 'bar')
+        for i in range(0, 60):
+            self.assertEqual(events[i]['summary'], curr_summ)
+            if(curr_summ == 'foo'):
+                curr_summ = 'bar'
+            else:
+                curr_summ = 'foo'
+
 
 class TestWebInterface(LocalTestCase):
 
