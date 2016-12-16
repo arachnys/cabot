116f456171ad6b1af398c0396748aa64e6660dfe
Elaine Arbaugh <elaine.arbaugh@affirm.com>
2016-12-16 14:23:54 -0800

    fix maestro.groovy

--- .jenkins/maestro.groovy
+++ .jenkins/maestro.groovy
@@ -5,14 +5,14 @@ def branch = env.get('ghprbSourceBranch') ?: env.get('GIT_BRANCH')
 // For maestro.pr should be null -> sha1 manual, and ghprbActualCommit automatic
 // so *should* work in all 4 needed cases
 def commit = env.get('ghprbActualCommit') ?: env.get('sha1')
-commit = commit ?" env.get('GIT_COMMIT')
+commit = commit ?: env.get('GIT_COMMIT')
 def channel = build.workspace.channel
 def results = []
 
 branch = branch - ~/^origin\//
 
 // set build name
-build.setDisplayName("${commit[0..9]}@{branch}")
+build.setDisplayName("${commit[0..9]}@${branch}")
 
 println "Building commit: ${commit} from branch ${branch} ..."
 
