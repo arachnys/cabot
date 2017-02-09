6ac8eb10ad380788b175bdaade22fbfba4662a3d
Frank Hamand <frankhamand@gmail.com>
2017-02-09 17:03:16 +0000

    Silence output from container waits and add timeout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
 set -e
 set -o allexport
 
 function wait_for_broker {(
   set +e
-  until python -c "from kombu import Connection; x=Connection('$CELERY_BROKER_URL', timeout=1); x.connect()"
-  do
-    echo 'Waiting for celery broker to respond...'
+  for try in {1..60} ;  do
+    python -c "from kombu import Connection; x=Connection('$CELERY_BROKER_URL', timeout=1); x.connect()" &> /dev/null && break
+    echo "Waiting for celery broker to respond..."
     sleep 1
   done
 )}
 
 function wait_for_database {(
   set +e
-  until python -c "from django.db import connection; connection.connect()"
-  do
-    echo 'Waiting for database to respond...'
+  for try in {1..60} ; do
+    python -c "from django.db import connection; connection.connect()" &> /dev/null && break
+    echo "Waiting for database to respond..."
     sleep 1
   done
 )}
