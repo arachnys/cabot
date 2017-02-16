9ccc9175c994a826d2db1b30f29e96deb12d69ea
Jannis Leidel <jannis@leidel.info>
2017-02-16 13:55:51 +0100

    Added missing default for environ_get_list

--- cabot/settings_utils.py
+++ cabot/settings_utils.py
@@ -1,6 +1,6 @@
 import os
 
-def environ_get_list(names, default):
+def environ_get_list(names, default=None):
     for name in names:
         if name in os.environ:
             return os.environ[name]
