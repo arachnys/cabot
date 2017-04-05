f99ef6d445924443d42cf4f572d318fcd05d8330
Frank Hamand <frankhamand@gmail.com>
2017-04-05 14:21:58 +0100

    Upgrade to django 1.11
    This requires a bump in django filter which needs django_filters adding
    to INSTALLED_APPS

--- cabot/settings.py
+++ cabot/settings.py
@@ -139,6 +139,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django_filters',
     'compressor',
     'polymorphic',
     'jsonify',
