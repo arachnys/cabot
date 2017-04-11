3a68bb5cdf53373c02c72fa4a092463a8e6e046c
Frank Hamand <frankhamand@gmail.com>
2017-04-11 18:26:21 +0100

    Change send_alert method to _send_alert proxy method
    This makes testing easier/better as we can mock the _send_alert method
    on the super class and not worry about mocking all the subclasses

--- cabot/cabotapp/alert.py
+++ cabot/cabotapp/alert.py
@@ -17,6 +17,20 @@ class AlertPlugin(PolymorphicModel):
     def __unicode__(self):
         return u'%s' % (self.title)
 
+    def _send_alert(self, service, users, duty_officers):
+        """
+        To allow easily monkey patching in hooks for all alerts.
+        e.g. mocking send_alert for all plugins in testing
+        """
+        return self.send_alert(service, users, duty_officers)
+
+    def _send_alert_update(self, service, users, duty_officers):
+        """
+        To allow easily monkey patching in hooks for all alerts.
+        e.g. mocking send_alert_update for all plugins in testing
+        """
+        return self.send_alert_update(service, users, duty_officers)
+
     def send_alert(self, service, users, duty_officers):
         """
         Implement a send_alert function here that shall be called.
@@ -39,7 +53,7 @@ def send_alert(service, duty_officers=None):
     users = service.users_to_notify.filter(is_active=True)
     for alert in service.alerts.all():
         try:
-            alert.send_alert(service, users, duty_officers)
+            alert._send_alert(service, users, duty_officers)
         except Exception as e:
             logging.exception('Could not send %s alert: %s' % (alert.name, e))
 
@@ -49,7 +63,7 @@ def send_alert_update(service, duty_officers=None):
     for alert in service.alerts.all():
         if hasattr(alert, 'send_alert_update'):
             try:
-                alert.send_alert_update(service, users, duty_officers)
+                alert._send_alert_update(service, users, duty_officers)
             except Exception as e:
                 logger.exception('Could not send %s alert update: %s' % (alert.name, e))
         else:
