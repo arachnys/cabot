6a9eff8f64bd7322684f51a3a8868e21099393d4
Jean-Frédéric <JeanFred@users.noreply.github.com>
2017-09-28 11:15:50 +0100

    Add Bash linting using bashate (#566)
    bashate is "a pep8 equivalent for bash scripts" by OpenStack.
    https://docs.openstack.org/bashate/latest/
    
    - Add tox target running bashate on the entire code-base
    - Fix docker-entrypoint script accordingly

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -e
 
-function wait_for_broker {(
+function wait_for_broker {
   set +e
   for try in {1..60} ;  do
     python -c "from kombu import Connection; x=Connection('$CELERY_BROKER_URL', timeout=1); x.connect()" && break
     echo "Waiting for celery broker to respond..."
     sleep 1
   done
-)}
+}
 
-function wait_for_database {(
+function wait_for_database {
   set +e
   for try in {1..60} ; do
     python -c "from django.db import connection; connection.connect()" && break
     echo "Waiting for database to respond..."
     sleep 1
   done
-)}
+}
 
-function wait_for_migrations {(
+function wait_for_migrations {
   set +e
   for try in {1..60} ; do
     # Kind of ugly but not sure if there's another way to determine if migrations haven't run.
@@ -28,7 +28,7 @@ function wait_for_migrations {(
     echo "Waiting for database migrations to be run..."
     sleep 1
   done
-)}
+}
 
 
 wait_for_broker
